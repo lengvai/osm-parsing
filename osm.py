ae532fb4e58263c2f1768bb72e74825c9efb04b5
lengvai <linas.rudelis@gmail.com>
2016-06-28 11:28:13 +0300

    Update osm.py

--- osm.py
+++ osm.py
@@ -67,96 +67,82 @@ cur.executescript('''
 		(refID INTEGER ,
 		way_id PRIMARY KEY )		
 		''')
-
-for name in os.listdir("path"): 
-			context=etree.iterparse(file)
-			for event, elem in context:
-				if elem.tag=='node':
-					nodeID=elem.get('id')
-					lat=elem.get('lat')
-					lon=elem.get('lon')		
-					cur.execute('INSERT OR IGNORE INTO Node (nodeID, lat, lon) VALUES ( ?, ?, ? )',( nodeID, lat, lon ) )
-					#last = cur.lastrowid
-					#cur.execute('SELECT id FROM Node WHERE nodeID = ? ', (last, ))
-					cur.execute('SELECT id FROM Node WHERE nodeID = ? ', (nodeID, ))
-					node_id = cur.fetchone()[0]							
-					for child in elem:
-						key=child.get('k')	
-						val=child.get('v')			
-						if key is None or val is None:	None
-						else: 
-							cur.execute('INSERT OR IGNORE INTO Key ( k ) VALUES ( ? )',( key, ) )
-							cur.execute('SELECT id FROM Key WHERE k = ? ', (key, ))
-							key_id = cur.fetchone()[0]		
+count=0
+for name in os.listdir("D:\Torrent\FDM\DE"): 
+        if name.endswith('.osm'):
+		context=etree.iterparse(file)
+		for event, elem in context:
+			if elem.tag=='node':
+				nodeID=elem.get('id')
+				lat=elem.get('lat')
+				lon=elem.get('lon')		
+				cur.execute('INSERT OR IGNORE INTO Node (nodeID, lat, lon) VALUES ( ?, ?, ? )',( nodeID, lat, lon ) )
+				cur.execute('SELECT id FROM Node WHERE nodeID = ? ', (nodeID, ))
+				node_id = cur.fetchone()[0]							
+				for child in elem:
+					key=child.get('k')	
+					val=child.get('v')			
+					if key is None or val is None:	None
+					else: 
+						cur.execute('INSERT OR IGNORE INTO Key ( k ) VALUES ( ? )',( key, ) )
+						cur.execute('SELECT id FROM Key WHERE k = ? ', (key, ))
+						key_id = cur.fetchone()[0]		
 							
-							cur.execute('INSERT OR IGNORE INTO Val ( v ) VALUES ( ? )',( val, ) )
-							cur.execute('SELECT id FROM Val WHERE v = ? ', (val, ))
-							val_id = cur.fetchone()[0]
+						cur.execute('INSERT OR IGNORE INTO Val ( v ) VALUES ( ? )',( val, ) )
+						cur.execute('SELECT id FROM Val WHERE v = ? ', (val, ))
+						val_id = cur.fetchone()[0]
 							
-							cur.execute('INSERT OR IGNORE INTO Node_tags ( val_id,key_id,node_id ) VALUES ( ?,?,? )',( val_id,key_id,node_id, ) )	
-							#cur.execute('INSERT OR IGNORE INTO Node_tags ( val_id,key_id,node_id ) VALUES ( ?,?,? )',( val_id,key_id,last, ) )
-					try:
-						child.clear()
-						elem.clear()
-					except: None
-				if elem.tag=='way':
-					wayID=elem.get('id')
-					cur.execute('INSERT OR IGNORE INTO Way (wayID) VALUES ( ? )',( wayID, ) )
-					cur.execute('SELECT id FROM Way WHERE wayID = ? ', (wayID, ))
-					way_id = cur.fetchone()[0]			
-					for child in elem:
-						ref=child.get('ref')
-						key=child.get('k')	
-						val=child.get('v')
-						if ref is None :	None
-						else:
-							cur.execute('INSERT OR IGNORE INTO Ref ( refID,way_id ) VALUES ( ?,? )',( ref,way_id, ) )
-						if key is None or val is None:	None
-						else: 
-							cur.execute('INSERT OR IGNORE INTO Key ( k ) VALUES ( ? )',( key, ) )
-							cur.execute('SELECT id FROM Key WHERE k = ? ', (key, ))
-							key_id = cur.fetchone()[0]		
+						cur.execute('INSERT OR IGNORE INTO Node_tags ( val_id,key_id,node_id ) VALUES ( ?,?,? )',( val_id,key_id,node_id, ) )	
+					
+			if elem.tag=='way':
+				wayID=elem.get('id')
+				cur.execute('INSERT OR IGNORE INTO Way (wayID) VALUES ( ? )',( wayID, ) )
+				cur.execute('SELECT id FROM Way WHERE wayID = ? ', (wayID, ))
+				way_id = cur.fetchone()[0]			
+				for child in elem:
+					ref=child.get('ref')
+					key=child.get('k')	
+					val=child.get('v')
+					if ref is None :	None
+					else:
+						cur.execute('INSERT OR IGNORE INTO Ref ( refID,way_id ) VALUES ( ?,? )',( ref,way_id, ) )
+					if key is None or val is None:	None
+					else: 
+						cur.execute('INSERT OR IGNORE INTO Key ( k ) VALUES ( ? )',( key, ) )
+						cur.execute('SELECT id FROM Key WHERE k = ? ', (key, ))
+						key_id = cur.fetchone()[0]		
 							
-							cur.execute('INSERT OR IGNORE INTO Val ( v ) VALUES ( ? )',( val, ) )
-							cur.execute('SELECT id FROM Val WHERE v = ? ', (val, ))
-							val_id = cur.fetchone()[0]
+						cur.execute('INSERT OR IGNORE INTO Val ( v ) VALUES ( ? )',( val, ) )
+						cur.execute('SELECT id FROM Val WHERE v = ? ', (val, ))
+						val_id = cur.fetchone()[0]
+						
+						cur.execute('INSERT OR IGNORE INTO Way_tags ( val_id,key_id,way_id ) VALUES ( ?,?,? )',( val_id,key_id,way_id, ) )
+			
+			if elem.tag=='relation':
+				relationID=elem.get('id')
+				cur.execute('INSERT OR IGNORE INTO Relation (relationID) VALUES ( ? )',( relationID, ) )
+				cur.execute('SELECT id FROM Relation WHERE relationID = ? ', (relationID, ))
+				relation_id = cur.fetchone()[0]	
+				for child in elem:			
+					key=child.get('k')	
+					val=child.get('v')	
+					if key is None or val is None:	None
+					else: 
+						cur.execute('INSERT OR IGNORE INTO Key ( k ) VALUES ( ? )',( key, ) )
+						cur.execute('SELECT id FROM Key WHERE k = ? ', (key, ))
+						key_id = cur.fetchone()[0]		
+						
+						cur.execute('INSERT OR IGNORE INTO Val ( v ) VALUES ( ? )',( val, ) )
+						cur.execute('SELECT id FROM Val WHERE v = ? ', (val, ))
+						val_id = cur.fetchone()[0]
 							
-							cur.execute('INSERT OR IGNORE INTO Way_tags ( val_id,key_id,way_id ) VALUES ( ?,?,? )',( val_id,key_id,way_id, ) )
-					try:
-						child.clear()
-						elem.clear()
-					except: None
-				if elem.tag=='relation':
-					relationID=elem.get('id')
-					cur.execute('INSERT OR IGNORE INTO Relation (relationID) VALUES ( ? )',( relationID, ) )
-					cur.execute('SELECT id FROM Relation WHERE relationID = ? ', (relationID, ))
-					relation_id = cur.fetchone()[0]	
-					for child in elem:			
-						key=child.get('k')	
-						val=child.get('v')	
-						if key is None or val is None:	None
-						else: 
-							cur.execute('INSERT OR IGNORE INTO Key ( k ) VALUES ( ? )',( key, ) )
-							cur.execute('SELECT id FROM Key WHERE k = ? ', (key, ))
-							key_id = cur.fetchone()[0]		
-							
-							cur.execute('INSERT OR IGNORE INTO Val ( v ) VALUES ( ? )',( val, ) )
-							cur.execute('SELECT id FROM Val WHERE v = ? ', (val, ))
-							val_id = cur.fetchone()[0]
-							
-							cur.execute('INSERT OR IGNORE INTO Relation_tags ( val_id,key_id,relation_id ) VALUES ( ?,?,? )',( val_id,key_id,relation_id, ) )
-					try:
-						child.clear()
-						elem.clear()
-					except: None		
-				count+=1
+						cur.execute('INSERT OR IGNORE INTO Relation_tags ( val_id,key_id,relation_id ) VALUES ( ?,?,? )',( val_id,key_id,relation_id, ) )
+					
+			count+=1
 				
-				if count %300000==0:
-					print count,("--- %s seconds ---" % (time.time() - start_time))					
-					conn.commit()
-
+			if count %300000==0:
+				print count,("--- %s seconds ---" % (time.time() - start_time))					
+				conn.commit()
 conn.close()
-
-	
 
 print 'ALL DONE!',("--- %s seconds ---" % (time.time() - start_time))
